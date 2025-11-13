ad3dfb3f156945dad6ffd2299d9a5d8fbc24db0f
Jared Brewer <jaredbrewersellshomes@gmail.com>
2025-11-12 20:49:38 -0500

    Complete Task D: Add routing and map component

--- d280-app/src/app/app.routes.ts
+++ d280-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MapComponent } from './pages/map/map';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'map',
+    pathMatch: 'full'
+  },
+  {
+    path: 'map',
+    component: MapComponent
+  }
+];
