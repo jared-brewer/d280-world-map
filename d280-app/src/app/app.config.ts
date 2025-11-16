dca7239581b66048372a495757d5bf763ff94059
Jared Brewer <jaredbrewersellshomes@gmail.com>
2025-11-16 08:52:19 -0500

    Completed Task G with API Integration

--- d280-app/src/app/app.config.ts
+++ d280-app/src/app/app.config.ts
@@ -1,3 +1,5 @@
+import { provideHttpClient } from '@angular/common/http';
+
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -7,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
--- d280-app/src/app/pages/map/map.ts
+++ d280-app/src/app/pages/map/map.ts
@@ -6,6 +6,10 @@
 // 5. latitude
 // 6. longitude 
 
+import { HttpClient } from '@angular/common/http';
+
+import { inject } from '@angular/core';
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -16,6 +20,8 @@ import { Component } from '@angular/core';
 export class MapComponent {
   selectedCountry: any = null;
 
+  http = inject(HttpClient);
+
   onCountryClick(event: MouseEvent) {
     const element = event.target as SVGPathElement;
 
@@ -24,13 +30,31 @@ export class MapComponent {
 
     console.log('Clicked country:', countryName, countryCode);
 
-    this.selectedCountry = {
-      name: countryName || '',
-      capitalCity: '',
-      region: { value: '' },
-      incomeLevel: { value: '' },
-      latitude: '',
-      longitude: ''
-    };
+    if (!countryCode) {
+      console.warn('No country code found on clicked element.');
+      return;
   }
+
+  const url = `https://api.worldbank.org/v2/country/${countryCode}?format=json`;
+
+  this.http.get<any>(url).subscribe({
+    next: (response) => {
+      const data = response[1][0]; // World Bank puts data in response[1]
+
+      this.selectedCountry = {
+        name: data.name,
+        capitalCity: data.capitalCity,
+        region: { value: data.region.value },
+        incomeLevel: { value: data.incomeLevel.value },
+        latitude: data.latitude,
+        longitude: data.longitude
+      };
+
+      console.log('API Data:', this.selectedCountry);
+    },
+    error: (err) => {
+      console.error('API error:', err);
+    }
+  });
+}
 }
